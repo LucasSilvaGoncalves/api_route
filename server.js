82705632d027aafa2221408c7061716c656bdb44
Lucas Goncalves <lucas.silvagoncalves@hotmail.com>
2020-11-25 17:14:54 -0300

    deploy

--- server.js
+++ server.js
@@ -2,25 +2,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 const { MONGO_URI } = require('./config');
-const cron = require('node-cron');
 const axios = require('axios');
 require('dotenv').config();
-
-// Health Check para manter a API funcionando no Heroku
-HealthCheck();
-
-function HealthCheck() {
-    console.log('HealthCheck API (Heroku)');
-    cron.schedule("*/10 * * * *", () =>
-        axios({
-            method: 'get',
-            url: 'https://digitaly-roteador-api.herokuapp.com/api/route'
-        })
-        .then(function (response) {
-            console.log("Executando HealthCheck a cada 10 minutos")
-        })
-    );
-}
 
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
